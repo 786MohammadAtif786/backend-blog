ca3242ed6997cfa6f8ba087647b8e8d0de6fcc40
Mohammad Atif <mohd.atif81999@gmail.com>
2026-03-24 12:07:49 +0530

    fix: send email url change

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -42,7 +42,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 export const sendEmail = async (email, token) => {
   try {
     //const verifyUrl = `${process.env.FRONTEND_URL}/verify/${token}`;
-    const url = "https://frontend-blog-alpha-ten.vercel.app/"
+    const url = "https://frontend-blog-alpha-ten.vercel.app"
     const verifyUrl = `${url}/verify/${token}`
     const msg = {
       to: email,

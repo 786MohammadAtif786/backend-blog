ea74a4fc920f7fbb4bcbb8fb3abb345760f55cbc
Mohammad Atif <mohd.atif81999@gmail.com>
2026-04-25 13:14:28 +0530

    fix: forgot password email is not found

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -657,7 +657,7 @@ export const forgotPassword = async (req, res) => {
         const user = await User.findOne({ email });
 
         if (!user) {
-            return res.status(404).json({ message: "User not found" });
+            return res.status(404).json({ message: "Email not found" });
         }
 
         const token = crypto.randomBytes(32).toString("hex");
